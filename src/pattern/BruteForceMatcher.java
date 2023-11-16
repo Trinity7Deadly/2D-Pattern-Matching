c080bc7d03c419e0fcca99c121ca97cc8fac2b5b
Fionn <psychedover@gmail.om>
2023-11-16 00:38:36 -0800

    Finished BruteForce I think... it runs.

--- src/pattern/BruteForceMatcher.java
+++ src/pattern/BruteForceMatcher.java
@@ -1,8 +1,38 @@
 package pattern;
 
 public class BruteForceMatcher implements Matcher {
-    @Override
+
+    // Find pattern match
     public int[] match(int[][] pattern, int[][] text) {
-        return new int[0];
+        int pL = pattern.length ;
+        int tL = text.length ;
+
+        // Iterate through all options
+        for (int i = 0; i <= tL - pL; i++) {
+            for (int j = 0; j <= tL - pL; j++) {
+                // Check if for match
+                if (bruteForce(pattern, text, i, j)) {
+                    return new int[]{i, j} ; // Return match position
+                }
+            }
+        }
+
+        return null ; // No match found
+    }
+
+    // Brute force comparison
+    private boolean bruteForce(int[][] pattern, int[][] text, int i, int j) {
+        int pL = pattern.length ;
+
+        // Run through elements
+        for (int a = 0; a < pL; a++) {
+            for (int b = 0; b < pL; b++) {
+                // Compare each element
+                if (pattern[a][b] != text[a + i][b + j]) {
+                    return false ; // If not a match
+                }
+            }
+        }
+        return true ; // If all match
     }
 }
